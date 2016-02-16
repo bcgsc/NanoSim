303e63c4da8238737cd40c4496369d29e11072a6
cheny19 <cheny@bcgsc.ca>
2016-02-16 11:55:35 -0800

    Create example.sh

---
+++ example.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#####
+# This script shows how the simulated reads are generated in the paper.
+# All training datasets are downloaded from ENA and processed with poretools.
+# Only 2D pass reads are extracted and stored on our ftp server.
+#####
+
+# 1. E. coli R7 dataset
+# Origin: 
