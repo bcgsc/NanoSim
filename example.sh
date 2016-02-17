743a1c654065237d03d8a7f6b9d72c9c08bec28b
cheny19 <cheny@bcgsc.ca>
2016-02-16 16:58:34 -0800

    Update example.sh

--- example.sh
+++ example.sh
@@ -3,8 +3,26 @@
 #####
 # This script shows how the simulated reads are generated in the paper.
 # All training datasets are downloaded from ENA and processed with poretools.
-# Only 2D pass reads are extracted and stored on our ftp server.
+# Only 2D pass reads are extracted and stored on bcgsc ftp server.
 #####
 
+# Download the source file
+
+# Create a working directory
+mkdir ecoli_simulation
+cd ecoli_simulation
+
 # 1. E. coli R7 dataset
-# Origin: 
+# Origin: ftp://climb.genomics.cn/pub/10.5524/100001_101000/100102/Ecoli_R7_CombinedFasta.tgz
+
+# Get the 2D reads
+wget ftp://ftp.bcgsc.ca/supplementary/NanoSim/*
+# Get the reference genome
+wget ftp://ftp.bcgsc.ca/supplementary/NanoSim/*
+
+# Profiling stage
+
+
+# To get the profile directly:
+wget ftp://ftp.bcgsc.ca/supplementary/NanoSim/ecoli_R7_profile.zip
+
