7edcdf3fed5902740d69bc517d4ee84a46ea426e
JustinChu <cjustin@bcgsc.ca>
2016-04-28 17:44:27 -0700

    bugfix for when max_length of read <25000 - Needs review

--- src/head_align_tail_dist.py
+++ src/head_align_tail_dist.py
@@ -22,6 +22,7 @@ def head_align_tail(outfile):
 
     aligned = []
     total = []
+    ht_total = []
     ht_ratio = {
         (0, 10): [], (10, 20): [], (20, 30): [], (30, 40): [], (40, 50): [], (50, 100): [], (100, 300): [], (300, 1000): [],
         (1000, 2000): [], (2000, 3000): [], (3000, 5000): [], (5000, 7500): [], (7500, 10000): [], (10000, "Inf"): []}
@@ -41,6 +42,7 @@ def head_align_tail(outfile):
             # tail = int(new[5])-int(new[2])-int(new[3])
             total.append(int(new[5]))
             ht = int(new[5])-int(new[3])
+            ht_total.append(ht)
             ratio = float(new[3])/float(new[5])
             for key in align_ratio.keys():
                 if key[0] <= int(new[3]) < key[1]:
@@ -54,15 +56,32 @@ def head_align_tail(outfile):
                     break
 
     max_length = max(total)
-
+    max_ht_length = max(ht_total)
+    
+    #remove keys that are larger than max_length
+    a_ratio_keys = align_ratio.keys()
+    for pair in a_ratio_keys:
+        lowerKey = pair[0]
+        if lowerKey >= max_length:
+            del align_ratio[pair]
+    
+    #remove keys that are larger than max_length
+    ht_ratio_keys = ht_ratio.keys()
+    for pair in ht_ratio_keys:
+        lowerKey = pair[0]
+        if lowerKey >= max_ht_length:
+            del ht_ratio[pair]
+        
     # The last key in align_ratio and ht_ratio are to Inf, needs to be replaced with the maximum length
     last_key = sorted(align_ratio.keys())[-1]
-    align_ratio[(last_key[0], max_length)] = align_ratio[last_key]
+    last_values = align_ratio[last_key]
     del align_ratio[last_key]
+    align_ratio[(last_key[0], max_length)] = last_values
 
     last_key = sorted(ht_ratio.keys())[-1]
-    ht_ratio[(last_key[0], max_length)] = ht_ratio[last_key]
+    last_values = ht_ratio[last_key]
     del ht_ratio[last_key]
+    ht_ratio[(last_key[0], max_ht_length)] = last_values
 
     # ecdf of align ratio
     align_cum = dict.fromkeys(align_ratio.keys(), [])
