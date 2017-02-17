acf92d4301c36474fd5f116dcf6eacb46f3a5f28
Karel Brinda <karel.brinda@gmail.com>
2017-02-17 15:18:05 -0500

    Fix Python 3 problem with .values()

--- src/simulator.py
+++ src/simulator.py
@@ -365,8 +365,8 @@ def reverse_complement(seq):
 def extract_read(dna_type, length):
     global seq_dict, seq_len, genome_len
 
-    if length > max(seq_dict.values()):
-        length = max(seq_dict.values())
+    if length > max(list(seq_dict.values())):
+        length = max(list(seq_dict.values()))
 
     # Extract the aligned region from reference
     if dna_type == "circular":
