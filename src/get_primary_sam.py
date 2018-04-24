3de39c9b78f3d6b27cf2dd2f43503b20e21c18d7
SaberHQ <saber.hafezqorani@gmail.com>
2018-04-23 17:28:38 -0700

    Copy and process provided SAM alignment file to outfile folder.

--- src/get_primary_sam.py
+++ src/get_primary_sam.py
@@ -5,15 +5,21 @@ import HTSeq
 
 
 def primary_and_unaligned(sam_alnm_file, outfile):
-    out1 = open(outfile + "_primary.sam", 'w')
+    out_sam = outfile + ".sam"
+    if out_sam == sam_alnm_file:
+        out_sam = outfile + "_processed.sam"
+    out1 = open(out_sam, 'w')
+    out2 = open(outfile + "_primary.sam", 'w')
     unaligned_dict = []
     sam_reader = HTSeq.SAM_Reader
     alignments = sam_reader(sam_alnm_file)
     for alnm in alignments:
+        out1.write(alnm.original_sam_line)
         if alnm.aligned and not alnm.not_primary_alignment and not alnm.supplementary:
-            out1.write(alnm.original_sam_line)
+            out2.write(alnm.original_sam_line)
         else:
             unaligned_dict.append(len(alnm.read.seq))
 
     out1.close()
+    out2.close()
     return unaligned_dict
