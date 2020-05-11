e3da52a9944e919e9f1f585796ab0d7960367ba4
Theodora Lo <tlo@hpce705.hpc.bcgsc.ca>
2020-05-11 15:22:19 -0700

    Simulate polyA tails and base qualities

--- src/mixed_model.py
+++ src/mixed_model.py
@@ -7,7 +7,7 @@ This script defines Poisson-Geometric distribution and Weibull-Geometric distrib
 
 import numpy as np
 from math import ceil
-from scipy.stats import rv_discrete, poisson, geom
+from scipy.stats import rv_discrete, poisson, geom, lognorm
 
 
 # Scipy geometric starts with x = 1
@@ -38,6 +38,20 @@ class weigeom2_gen(rv_discrete):
         return self.cdf(x, l, k, p, w) - self.cdf(x-1, l, k, p, w)
 
 
+class trunc_lognorm_gen(rv_discrete):
+    def _cdf(self, x, s, m):
+        lncdf_x = lognorm.cdf(x, s, scale=m)
+        lncdf_a = lognorm.cdf(self.a, s, scale=m)
+        lncdf_b = lognorm.cdf(self.b, s, scale=m)
+        return (lncdf_x - lncdf_a) / (lncdf_b - lncdf_a)
+
+    def _ppf(self, q, s, m):
+        lncdf_a = lognorm.cdf(self.a, s, scale=m)
+        lncdf_b = lognorm.cdf(self.b, s, scale=m)
+        ln_q = q * (lncdf_b - lncdf_a) + lncdf_a
+        return lognorm.ppf(ln_q, s, scale=m)
+
+
 def pois_geom(lam, prob, weight):
     # Draw a random number from Poisson-Geometric distribution
     # Faster to use numpy random than using Scipy rvs
@@ -61,3 +75,112 @@ def wei_geom(lam, k, prob, weight):
         value = 1
 
     return value
+
+
+def trunc_lognorm_rvs(error_type, read_type, basecaller, n):
+    if basecaller == "albacore":
+        a = 1
+        b = 28
+        if read_type == "DNA":
+            if error_type == "match":
+                mean = 2.7418286
+                sd = 0.7578693
+            elif error_type == "mis":
+                mean = 1.6597215
+                sd = 0.6814804
+            elif error_type == "ins":
+                mean = 1.9016147
+                sd = 0.6842999
+            elif error_type == "ht":
+                mean = 2.3739153
+                sd = 0.9635895
+            else:  # unaligned
+                mean = 2.5484921
+                sd = 0.7742894
+        elif read_type == "dRNA":
+            if error_type == "match":
+                mean = 2.236641
+                sd = 0.434045
+            elif error_type == "mis":
+                mean = 1.8138169
+                sd = 0.4535039
+            elif error_type == "ins":
+                mean = 1.9322685
+                sd = 0.4668444
+            elif error_type == "ht":
+                mean = 2.0166876
+                sd = 0.5714308
+            else:  # unaligned
+                mean = 2.1371272
+                sd = 0.4763441
+        else:  # cDNA
+            if error_type == "match":
+                mean = 2.6003978
+                sd = 0.7181057
+            elif error_type == "mis":
+                mean = 1.6380338
+                sd = 0.6695235
+            elif error_type == "ins":
+                mean = 1.8462438
+                sd = 0.6661691
+            elif error_type == "ht":
+                mean = 2.510699
+                sd = 1.082626
+            else:  # unaligned
+                mean = 2.6004634
+                sd = 0.8526468
+    elif basecaller == "guppy":
+        a = 1
+        b = 31
+        if read_type == "DNA":
+            if error_type == "match":
+                mean = 2.9863022
+                sd = 0.9493498
+            elif error_type == "mis":
+                mean = 1.6184245
+                sd = 0.7585733
+            elif error_type == "ins":
+                mean = 1.8852560
+                sd = 0.7623103
+            elif error_type == "ht":
+                mean = 1.995397
+                sd = 1.008650
+            else:  # unaligned
+                mean = 1.2626728
+                sd = 0.9012829
+        elif read_type == "dRNA":
+            if error_type == "match":
+                mean = 2.236641
+                sd = 0.434045
+            elif error_type == "mis":
+                mean = 1.8138169
+                sd = 0.4535039
+            elif error_type == "ins":
+                mean = 1.9322685
+                sd = 0.4668444
+            elif error_type == "ht":
+                mean = 2.0166876
+                sd = 0.5714308
+            else:  # unaligned
+                mean = 2.1371272
+                sd = 0.4763441
+        else:  # cDNA
+            if error_type == "match":
+                mean = 2.7500148
+                sd = 0.9195383
+            elif error_type == "mis":
+                mean = 1.5543628
+                sd = 0.7601223
+            elif error_type == "ins":
+                mean = 1.765634
+                sd = 0.777587
+            elif error_type == "ht":
+                mean = 2.001173
+                sd = 1.008647
+            else:  # unaligned
+                mean = 1.2635415
+                sd = 0.9008419
+
+    m = exp(mean)
+    truncln_gen = trunc_lognorm_gen(name="truncln", a=a, b=b, shapes="s, m")
+    return truncln_gen.rvs(s=sd, m=m, size=n)
