30742ccb06b74aed26eeb1853ead5bf259cb2ec8
Tham Cheng Yong <25361260+cytham@users.noreply.github.com>
2020-06-20 23:55:38 +0800

    Suggested HTSeq v0.12.4 compatibility changes
    -Added header to prefix_primary.sam file
    -Substituted original_sam_line with get_sam_line() because original_sam_line returns empty string
    Note: For it to work, we need to wait for HTSeq to resolve issue https://github.com/htseq/htseq/issues/11 in commit 797f802

--- src/get_primary_sam.py
+++ src/get_primary_sam.py
@@ -3,6 +3,7 @@
 from __future__ import with_statement
 import HTSeq
 import numpy
+import pysam
 
 
 def primary_and_unaligned(sam_alnm_file, prefix):
@@ -10,11 +11,13 @@ def primary_and_unaligned(sam_alnm_file, prefix):
     unaligned_len = []
     pos_strand = 0
     num_aligned = 0
+    samfile = pysam.AlignmentFile(sam_alnm_file, "r")  # Read sam file using pysam for extracting sam header
+    out_primary.write(str(samfile.header))  # Write sam header into out_primary
     sam_reader = HTSeq.SAM_Reader
     alignments = sam_reader(sam_alnm_file)
     for alnm in alignments:
         if alnm.aligned and (not alnm.not_primary_alignment and not alnm.supplementary):
-            out_primary.write(alnm.original_sam_line)
+            out_primary.write(alnm.get_sam_line() + '\n')  # Use get_sam_line() instead of original_sam_line, and add a newline)
             num_aligned += 1
             if alnm.flag == 0:
                 pos_strand += 1
@@ -26,3 +29,4 @@ def primary_and_unaligned(sam_alnm_file, prefix):
     out_primary.close()
     unaligned_len = numpy.array(unaligned_len)
     return unaligned_len, strandness
+
