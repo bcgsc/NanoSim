36d3c4544eea399a967c0cd28d65d7e4ecd6548f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-19 14:22:39 +0800

    Delete src directory

--- src/file_handler.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import gzip
-
-# function to open both gzip'd and regular files
-def gzopen(file_path, mode='rt', compresslevel=6):
-    if file_path.lower().endswith('.gz'):
-        return gzip.open(file_path, mode=mode, compresslevel=compresslevel)
-    return open(file_path, mode)
